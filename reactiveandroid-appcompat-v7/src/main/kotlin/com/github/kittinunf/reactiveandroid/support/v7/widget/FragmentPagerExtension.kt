c271ee1f01adba07c2a63053ec2c53e01b1e84e6
Tipatai Puthanukunkit <tipatai.p@taskworld.com>
2016-05-03 11:16:21 +0700

    add fragment pager extension

---
+++ reactiveandroid-appcompat-v7/src/main/kotlin/com/github/kittinunf/reactiveandroid/support/v7/widget/FragmentPagerExtension.kt
@@ -0,0 +1,66 @@
+package com.github.kittinunf.reactiveandroid.support.v7.widget
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import rx.Observable
+import rx.Subscription
+
+/**
+ * Created by tipatai on 5/3/16 AD.
+ */
+
+abstract class FragmentPagerProxyAdapter<ARG>(fragmentManager: FragmentManager) : FragmentPagerAdapter (fragmentManager) {
+
+    internal var items: List<ARG> = listOf()
+
+    open var pageWidth: ((Int) -> Float)? = null
+
+    open var pageTitle: ((Int, ARG) -> String)? = null
+
+    abstract var item: ((Int, ARG) -> Fragment)
+
+    override fun getItem(position: Int): Fragment? = item.invoke(position, items[position])
+
+    override fun getPageWidth(position: Int): Float =
+            pageWidth?.let {
+                it.invoke(position)
+            } ?: {
+                super.getPageWidth(position)
+            }()
+
+    override fun getPageTitle(position: Int): CharSequence? =
+            pageTitle?.let {
+                it.invoke(position, items[position])
+            } ?: {
+                super.getPageTitle(position)
+            }()
+
+    override fun getCount(): Int = items.size
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+}
+
+fun <ARG, L : List<ARG>> ViewPager.rx_fragmentsWith(observable: Observable<L>, fragmentManager: FragmentManager,
+                                                    getItem: (Int, ARG) -> Fragment,
+                                                    getPageTitle: ((Int, ARG) -> String)? = null,
+                                                    getPageWidth: ((Int) -> Float)? = null): Subscription {
+    val proxyAdapter = object : FragmentPagerProxyAdapter<ARG>(fragmentManager) {
+
+        override var item: (Int, ARG) -> Fragment = getItem
+        override var pageTitle: ((Int, ARG) -> String)? = getPageTitle
+        override var pageWidth: ((Int) -> Float)? = getPageWidth
+
+    }
+    return rx_fragmentsWith(observable, proxyAdapter)
+}
+
+fun <ARG, ADT : FragmentPagerProxyAdapter<ARG>, L : List<ARG>> ViewPager.rx_fragmentsWith(observable: Observable<L>, fragmentPagerProxyAdapter: ADT): Subscription {
+    adapter = fragmentPagerProxyAdapter
+    return observable.subscribe {
+        fragmentPagerProxyAdapter.items = it
+        fragmentPagerProxyAdapter.notifyDataSetChanged()
+    }
+}
---
+++ sample/src/main/java/com/github/kittinunf/reactiveandroid/sample/view/FragmentPagerActivity.kt
@@ -0,0 +1,39 @@
+package com.github.kittinunf.reactiveandroid.sample.view
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+
+import com.github.kittinunf.reactiveandroid.sample.R
+import com.github.kittinunf.reactiveandroid.support.v7.widget.rx_fragmentsWith
+import com.judrummer.androidbestrhythm.viewpager.section.PlaceholderFragment
+import kotlinx.android.synthetic.main.activity_fragment_pager.*
+import rx.Observable
+
+class FragmentPagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_pager)
+        setUpUI()
+    }
+
+    fun setUpUI() {
+        setSupportActionBar(toolbar)
+        viewPager.rx_fragmentsWith(Observable.just(listOf("section1", "section2", "section3")), supportFragmentManager,
+                { position, item ->
+                    PlaceholderFragment.newInstance(item)
+                },
+                { position, item ->
+                    item
+                },
+                { position ->
+                    0.9f
+                }
+        )
+    }
+
+}
