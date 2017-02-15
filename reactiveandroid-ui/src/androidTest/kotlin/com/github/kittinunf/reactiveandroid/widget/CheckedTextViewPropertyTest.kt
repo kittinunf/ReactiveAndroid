3586e3ab73b65a8e9e4624f445cbe49e107a9ce2
Kittinun Vantasin <kittinun.f@gmail.com>
2017-02-15 22:34:02 +0900

    Add more test for checkedTextView

---
+++ reactiveandroid-ui/src/androidTest/kotlin/com/github/kittinunf/reactiveandroid/widget/CheckedTextViewPropertyTest.kt
@@ -0,0 +1,51 @@
+package com.github.kittinunf.reactiveandroid.widget
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.annotation.UiThreadTest
+import android.support.test.rule.UiThreadTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.widget.CheckedTextView
+import com.github.kittinunf.reactiveandroid.rx.bindTo
+import com.github.kittinunf.reactiveandroid.scheduler.AndroidThreadScheduler
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.BeforeClass
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import rx.Observable
+import rx.schedulers.Schedulers
+
+@RunWith(AndroidJUnit4::class)
+class CheckedTextViewPropertyTest {
+
+    @Rule
+    @JvmField
+    val uiThreadTestRule = UiThreadTestRule()
+
+    private val context = InstrumentationRegistry.getContext()
+    private val instrument = InstrumentationRegistry.getInstrumentation()
+    private val view = CheckedTextView(context)
+
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun setUp() {
+            AndroidThreadScheduler.main = Schedulers.immediate()
+        }
+    }
+
+    @Test
+    @UiThreadTest
+    fun testOverflowIcon() {
+        val checked = view.rx_checked
+
+        Observable.just(true).bindTo(checked)
+        assertThat(view.isChecked, equalTo(true))
+
+        checked.bindTo(Observable.just(false))
+        assertThat(view.isChecked, equalTo(false))
+    }
+}
+
+
