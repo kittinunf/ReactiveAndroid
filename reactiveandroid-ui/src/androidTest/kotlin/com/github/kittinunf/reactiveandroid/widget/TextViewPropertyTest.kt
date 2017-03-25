63d82c4eb71488b3c7e8a9584b4074a58681024f
Kittinun Vantasin <kittinun.f@gmail.com>
2017-03-25 21:01:36 +0900

    Add test for TextView event

---
+++ reactiveandroid-ui/src/androidTest/kotlin/com/github/kittinunf/reactiveandroid/widget/TextViewPropertyTest.kt
@@ -0,0 +1,8 @@
+package com.github.kittinunf.reactiveandroid.widget
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TextViewPropertyTest {
+}
