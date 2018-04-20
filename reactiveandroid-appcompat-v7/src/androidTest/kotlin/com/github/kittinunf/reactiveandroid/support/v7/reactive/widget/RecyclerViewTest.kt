bd7d932e84a1bcbe149d14efe92cbf90f37548a1
Christopher Ng <babedroid@gmail.com>
2018-04-20 13:45:02 +0700

    :bug: fix test cases

--- reactiveandroid-appcompat-v7/src/androidTest/kotlin/com/github/kittinunf/reactiveandroid/support/v7/reactive/widget/RecyclerViewTest.kt
+++ reactiveandroid-appcompat-v7/src/androidTest/kotlin/com/github/kittinunf/reactiveandroid/support/v7/reactive/widget/RecyclerViewTest.kt
@@ -162,12 +162,12 @@ class RecyclerViewTest {
     fun adapter() {
         val expected = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-            override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
             }
 
             override fun getItemCount(): Int = 1
 
-            override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder =
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
                     object : RecyclerView.ViewHolder(View(instrumentation.context)) {}
         }
 
